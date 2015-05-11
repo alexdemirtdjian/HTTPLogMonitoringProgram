bc2d80651b3aa8cda79ea5eddeb4996d682943ae
Alexandre Bazbazian <alexandre.bazbazian@free.fr>
2015-05-11 11:52:54 +0200

    functions on cache and ip hits

--- LogMonitoring/hitsOnCache.py
+++ LogMonitoring/hitsOnCache.py
@@ -1,1 +1,18 @@
 __author__ = 'alexandre'
+
+# we will monitor how often client requests are refused because the client's cache
+# of the page is up to date
+
+def ClientCachePercentage(logfile_pathname):
+    logfile = open(logfile_pathname, "r").xreadlines()
+    # log files can be huge, we use xreadlines instead of readline in order
+    # not ot load all the lines in a list
+    TotalRequest = 0
+    CachedRequest = 0
+
+    for line in logfile:
+        TotalRequest += 1
+        if line.split(" ")[8] == "304":  # the server returned 'not modified'
+            CachedRequest += 1
+
+    return 100*(CachedRequest/TotalRequest)
--- LogMonitoring/hitsOnIp.py
+++ LogMonitoring/hitsOnIp.py
@@ -1,1 +1,20 @@
 __author__ = 'alexandre'
+
+# This file will check the number of hits from each individual IP address that accessed the machine
+# adpted from Oreilly : python cookbook
+
+
+from collections import defaultdict
+
+def CalculateIpHits(logfile_pathname):
+    # we will store the hits in a dictionary
+    # d[ip] = numberOfHits
+    IpHitDict = defaultdict()
+    Logfile = open(logfile_pathname, "r").xreadlines()
+    # We go then through each of the logfile
+    for line in Logfile:
+        # Split the string to isolate the IP address
+        Ip = line.split(" ")[0]
+        if (6 < len(Ip)) and (len(Ip) <= 15):  # we check the length of the ip adress
+            IpHitDict[Ip] += 1
+    return IpHitDict
