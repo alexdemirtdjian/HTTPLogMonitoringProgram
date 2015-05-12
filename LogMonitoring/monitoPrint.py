74b7463bc7c0d8e4712b61aacdf88cdf3234a87a
Alexandre Bazbazian <alexandre.bazbazian@free.fr>
2015-05-12 15:03:50 +0200

    the functions that will print alerts and stats on the screen

--- LogMonitoring/monitoPrint.py
+++ LogMonitoring/monitoPrint.py
@@ -4,3 +4,35 @@ __author__ = 'alexandre'
 # and also interesting stats
 # monitoPrint will read from the queue the events, analyzing it and then pretty printing on the console
 
+from collections import Counter
+
+
+threshold = 100  # this is the threshold at which the alert will be triggered
+
+
+def print_stats(count):
+    """
+    :param count: counter, which contains all the request from the last 10s
+    :return: unit : print stats from the requests
+    """
+    print count.most_common(3)  # we get the most common paths
+    print sum(count.values())  # we count all the requests from the last 10 s
+
+
+
+def monito_print(count, last_120_requests, alert_state):
+    """
+    :param count: counter, which contains all the request from the last 10s
+    :param last_120_request: list containing the request from the last 120 s
+    :param alert_state: bool, representing if we are are notin alert state
+    :return: unit : print on the screen the loggins
+    """
+    if alert_state:  # we were in alert state : did we recovered ?
+        if len(last_120_requests) > threshold:  # still alert state
+            print "alert state still on"
+        else:  # we recovered
+            print "alert recovery"
+    else:  # (no alert state)
+        if len(last_120_requests) > threshold:
+            print "alert state on"
+    print_stats(count)
