360b7e4ea6c98ae070fc9aaae431222be2f04509
Alexandre Bazbazian <alexandre.bazbazian@free.fr>
2015-05-12 22:49:52 +0200

    more code refactoring

--- LogMonitoring/hitsOnCache.py
+++ LogMonitoring/hitsOnCache.py
@@ -3,16 +3,17 @@ __author__ = 'alexandre'
 # we will monitor how often client requests are refused because the client's cache
 # of the page is up to date
 
-def ClientCachePercentage(logfile_pathname):
+
+def client_cache_percentage(logfile_pathname):
     logfile = open(logfile_pathname, "r").xreadlines()
     # log files can be huge, we use xreadlines instead of readline in order
     # not ot load all the lines in a list
-    TotalRequest = 0
-    CachedRequest = 0
+    total_request = 0
+    cached_request = 0
 
     for line in logfile:
-        TotalRequest += 1
+        total_request += 1
         if line.split(" ")[8] == "304":  # the server returned 'not modified'
-            CachedRequest += 1
+            cached_request += 1
 
-    return 100*(CachedRequest/TotalRequest)
+    return 100*(cached_request/total_request)
--- LogMonitoring/hitsOnIp.py
+++ LogMonitoring/hitsOnIp.py
@@ -1,20 +1,21 @@
 __author__ = 'alexandre'
 
 # This file will check the number of hits from each individual IP address that accessed the machine
-# adpted from Oreilly : python cookbook
+# adapted from Oreilly : python cookbook
 
 
 from collections import defaultdict
 
-def CalculateIpHits(logfile_pathname):
+
+def calculate_ip_hits(logfile_pathname):
     # we will store the hits in a dictionary
     # d[ip] = numberOfHits
-    IpHitDict = defaultdict()
-    Logfile = open(logfile_pathname, "r").xreadlines()
+    ip_hit_dict = defaultdict()
+    log_file = open(logfile_pathname, "r").xreadlines()
     # We go then through each of the logfile
-    for line in Logfile:
+    for line in log_file:
         # Split the string to isolate the IP address
-        Ip = line.split(" ")[0]
-        if (6 < len(Ip)) and (len(Ip) <= 15):  # we check the length of the ip adress
-            IpHitDict[Ip] += 1
-    return IpHitDict
+        ip = line.split(" ")[0]
+        if (6 < len(ip)) and (len(ip) <= 15):  # we check the length of the ip adress
+            ip_hit_dict[ip] += 1
+    return ip_hit_dict
